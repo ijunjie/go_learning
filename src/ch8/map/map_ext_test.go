24e94bb53e2e00f70e0feedb8328258ca8d3ec76
wangjunjie <wang-junjie@outlook.com>
2019-12-23 23:39:13 +0800

    ch8

---
+++ src/ch8/map/map_ext_test.go
@@ -0,0 +1,25 @@
+package mapext
+
+import "testing"
+
+/*
+ map 的 value 可以是 func
+*/
+func TestMapWithFunValue(t *testing.T) {
+	m := map[int]func(int) int{}
+	m[1] = func(op int) int { return op }
+	m[2] = func(op int) int { return op * op }
+	m[3] = func(op int) int { return op * op * op }
+	t.Log(m[1](2), m[2](2), m[3](2))
+}
+
+func TestMapSet(t *testing.T) {
+	mySet := map[int]bool{}
+	mySet[1] = false
+	n := 1
+	if v, f := mySet[n]; f {
+		t.Logf("%d existing. value is %v", n, v)
+	} else {
+		t.Logf("%d not existing", n)
+	}
+}
